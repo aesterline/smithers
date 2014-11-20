6b64259c6b7022e00375f54b7af676325407e439
Adam Esterline <adam@esterlines.com>
2014-11-19 21:22:50 -0700

    Initial syntax text editor

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.4"]]
+                 [seesaw "1.4.4"]
+                 [com.fifesoft/rsyntaxtextarea "2.5.3"]]
   :main ^:skip-aot smiters.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/smiters/core.clj
+++ src/smiters/core.clj
@@ -1,11 +1,27 @@
 (ns smiters.core
   (:gen-class)
+  (:import [org.fife.ui.rtextarea RTextScrollPane]
+           [org.fife.ui.rsyntaxtextarea RSyntaxTextArea SyntaxConstants])
   (:use seesaw.core))
+
+(defn- create-text-area []
+  (doto (RSyntaxTextArea. 20 60)
+    (.setSyntaxEditingStyle SyntaxConstants/SYNTAX_STYLE_CLOJURE)
+    (.setCodeFoldingEnabled true)
+    (.setAntiAliasingEnabled true)))
+
+(defn- create-scroll-pane []
+  (doto (RTextScrollPane. (create-text-area))
+    (.setFoldIndicatorEnabled true)))
+
+(defn- create-frame []
+  (let [panel (border-panel :center (create-scroll-pane))]
+    (frame :title "Text Editor Demo"
+           :on-close :exit
+           :content panel)))
 
 (defn -main [& args]
   (invoke-later
-    (-> (frame :title "Hello",
-               :content "Hello, Seesaw",
-               :on-close :exit)
+    (-> (create-frame)
         pack!
         show!)))
