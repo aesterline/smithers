66314baeb09be92e4c160fd28676f611201645be
Adam Esterline <adam@esterlines.com>
2014-11-20 21:46:46 -0700

    Added a function to display a file in the code editor.

--- src/smiters/core.clj
+++ src/smiters/core.clj
@@ -1,27 +1,41 @@
 (ns smiters.core
   (:gen-class)
+  (:require [seesaw.chooser :as chooser])
   (:import [org.fife.ui.rtextarea RTextScrollPane]
            [org.fife.ui.rsyntaxtextarea RSyntaxTextArea SyntaxConstants])
   (:use seesaw.core))
 
-(defn- create-text-area []
+(defn- create-code-area []
   (doto (RSyntaxTextArea. 20 60)
     (.setSyntaxEditingStyle SyntaxConstants/SYNTAX_STYLE_CLOJURE)
     (.setCodeFoldingEnabled true)
     (.setAntiAliasingEnabled true)))
 
-(defn- create-scroll-pane []
-  (doto (RTextScrollPane. (create-text-area))
-    (.setFoldIndicatorEnabled true)))
+(defn- display-code [ide]
+  (let [scroll-pane (doto (RTextScrollPane. (:code-area ide))
+                      (.setFoldIndicatorEnabled true))]
+    (config! (:ide ide) :content (border-panel :center scroll-pane))
+    ide))
 
-(defn- create-frame []
-  (let [panel (border-panel :center (create-scroll-pane))]
-    (frame :title "Text Editor Demo"
-           :on-close :exit
-           :content panel)))
+(defn create-editor [ide]
+  (-> ide
+      (assoc :code-area (create-code-area))
+      (display-code)))
+
+(defn show-file [app file]
+  (config! (:code-area app) :text (slurp file)))
+
+(defn display! [app]
+  (-> (:ide app)
+      pack!
+      show!))
+
+(defn create-ide []
+  (-> {:ide (frame :title "Text Editor Demo" :on-close :exit)}
+      create-editor))
 
 (defn -main [& args]
+  (native!)
   (invoke-later
-    (-> (create-frame)
-        pack!
-        show!)))
+    (-> (create-ide)
+        display!)))
